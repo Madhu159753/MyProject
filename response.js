092ae228189f962314eadab259cec80844aefb19
Madhu159753 <madhu01111998@gmail.com>
2023-01-01 00:59:46 +0530

    for showing data on screen

--- response.js
+++ response.js
@@ -5,27 +5,39 @@ const server=http.createServer((req,res)=>{
     const url=req.url;
    const method=req.method;
    if(url==='/'){
-    res.write('<html>');
-    res.write('<head><title>enter message</head></title>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
-    res.write('</html');
+    fs.readFile("message.txt",(err,data)=>{
+      if(err){
+        console.log(err);
+      }
+      res.write("<html>");
+    res.write("<head><title>enter message</head></title>");
+    res.write(`<body>${data}</body>`);
+    res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>`);
+    res.write("</html>");
     return res.end();
-
+  });
    }
-   if(url === '/message' && method === 'POST')
+  else if(url === '/message' && method === 'POST')
    {
-     fs.writeFileSync('me.txt', 'dummy');
-     res.statusCode=302;
-     res.setHeader('Location', '/');
-     return res.end();
-   }
-    
+    const body=[];
+    req.on('data',(chunk)=>{
+      console.log(chunk);
+     body.push(chunk);
+    });  
+     return req.on('end', () => {
+      const parseBody=Buffer.concat(body).toString();
+      const message=parseBody.split('=')[1];
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode=302;
+        res.setHeader('Location', '/');
+        return res.end(); 
+      });
+     });    
+   } 
     res.setHeader('content-type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first page</head></title>');
     res.write('<body><h1>Welcome to my node js projec</h1></body>');
     res.write('</html');
-   
-
-})
+});
 server.listen(3000);
