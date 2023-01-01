0905aebbdb6e8690f9afaaa5563b633331bc041d
Madhu159753 <madhu01111998@gmail.com>
2023-01-02 02:57:17 +0530

    adding form element in express

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,24 +1,19 @@
-// const http = require('http');
-// //const routes=require('./explore');
-// //console.log(routes.sometext);
-// //console.log(routes.few)
-// const express=require('express');
-// const app=express();
-// app.use((req,res,next)=>{
-// console.log('in the middle ware');
-// next();
-// });
-// const server=http.createServer(app);
-// server.listen(2000);
-//const http = require('http');
+
 const express=require('express');
+const parser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-console.log('in the middleweare');
-next();
+app.use(parser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send(`<body><form action="/product" method="POST"><input type="text" name="size"><input type="text" name="title"><button type="submit">SEND</button></form></body>`);
 });
-app.use((req,res,next)=>{
-console.log('in secondmiddleware');
+app.use('/product',(req,res,next)=>{
+
+    console.log(req.body);
+    res.redirect('/');
+
+});
+app.use('/',(req,res,next)=>{
 res.send({sort:'key'});
 });
 
